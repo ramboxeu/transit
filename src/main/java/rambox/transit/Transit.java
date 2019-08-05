5d6a95d679ed6b4f92b33313f943ea0932edfc24
RamboxEu <mail.rambox@gmail.com>
2019-08-05 18:26:19 +0200

    Add block and item base classes

--- src/main/java/rambox/transit/Transit.java
+++ src/main/java/rambox/transit/Transit.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 import rambox.transit.proxy.CommonProxy;
 
-@Mod(modid="transit")
+@Mod(modid=Transit.MOD_ID)
 public class Transit
 {
     @Mod.Instance
@@ -19,13 +19,14 @@ public class Transit
 
     public static Logger logger;
 
-    public static final String version = "0.1";
+    public static final String VERSION = "0.1";
+    public static final String MOD_ID = "transit";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
-        logger.debug("====[TRANSIT]====\nWelcome to Transit v{}\n\nMaking Minecraft more technical since 2019.================", version);
+        logger.debug("====[TRANSIT]====\nWelcome to Transit v{}\n\nMaking Minecraft more technical since 2019.================", VERSION);
     }
 
     @Mod.EventHandler
--- src/main/java/rambox/transit/proxy/CommonProxy.java
+++ src/main/java/rambox/transit/proxy/CommonProxy.java
@@ -1,12 +1,16 @@
 package rambox.transit.proxy;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import rambox.transit.Transit;
+import rambox.transit.block.TransitBlocks;
+import rambox.transit.item.TransitItems;
 
 @Mod.EventBusSubscriber
 public class CommonProxy
@@ -29,12 +33,20 @@ public class CommonProxy
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-
+        Transit.logger.info("Registering {} blocks!", TransitBlocks.BLOCKS.size());
+        for(Block block : TransitBlocks.BLOCKS)
+        {
+            event.getRegistry().register(block);
+        }
     }
 
     @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Block> event)
+    public static void registerItems(RegistryEvent.Register<Item> event)
     {
-
+        Transit.logger.info("Registering {} items!", TransitItems.ITEMS.size());
+        for(Item item : TransitItems.ITEMS)
+        {
+            event.getRegistry().register(item);
+        }
     }
 }
